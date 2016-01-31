9cfcce048f3158fadb4fb749a8a00328fd401094
Stacey Gammon <stacey@staceygammon.com>
2016-01-31 17:33:50 -0500

    Add two (dummy) pages in separate modules.  Use webpack to manage and compile modules.

---
+++ SeeSpotHelp/SeeSpotHelp/App/shelterhomepage.jsx
@@ -0,0 +1,13 @@
+﻿
+var ShelterHomePage = React.createClass({
+    render: function() {
+        return (
+          <div>
+            Shelter Home Page
+        {this.props.children}
+          </div>
+      );
+}
+});
+
+module.exports = ShelterHomePage;
