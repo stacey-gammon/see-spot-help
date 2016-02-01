28dd602555e279d764c65262a4ec874dd34e4efc
Stacey Gammon <stacey@staceygammon.com>
2016-01-31 22:59:56 -0500

    Revert "Revert "Save selected volunteer group in a session state as a temporary implementation.  Will need to add login abilities to save real session state (facebook, g+ login, etc).""
    This reverts commit 6bcf41127e7e19816a6b900c9db869eef8fea5f6.

--- SeeSpotHelp/SeeSpotHelp/App/shelterhomepage.jsx
+++ SeeSpotHelp/SeeSpotHelp/App/shelterhomepage.jsx
@@ -1,20 +1,33 @@
 ﻿'use strict'
 var React = require('react');
+var ShelterSearchBox = require('./sheltersearchbox');
+var FakeData = require('./fakedata');
 
 var ShelterHomePage = React.createClass({
     render: function () {
         var query = this.props.location.query;
-        var groupName = "Search for a shelter volunteer group to join!";
-        if (query && query.groupName) {
-            groupName = query.groupName;
+        if (query && query.groupId) {
+            var activeGroup = FakeData.GetFakeVolunteerGroupData()[query.groupId];
+            localStorage.setItem('activeGroup', JSON.stringify(activeGroup));
         }
-        return (
-          <div>
-            {groupName}
-        {this.props.children}
-          </div>
-      );
-}
+        var group = JSON.parse(localStorage.getItem('activeGroup'));
+        if (group) {
+            return (
+              <div>
+                <h1>{group.name}</h1>
+                  <h2>{group.shelterName} - {group.address}</h2>
+                  <hr/>
+            {this.props.children}
+              </div>
+          );
+        } else {
+            return (
+                <div>
+                    <ShelterSearchBox/>
+                </div>
+            );
+        }
+    }
 });
 
 module.exports = ShelterHomePage;
