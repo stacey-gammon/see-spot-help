a7441e5372fd2e0546d55c08123e69e207256c6a
Stacey Gammon <stacey@staceygammon.com>
2016-01-31 21:04:24 -0500

    Fill in search page and search fake data.  Display results.  Shelter result buttons don't do anything yet and styling is needed.

---
+++ SeeSpotHelp/SeeSpotHelp/App/sheltersearch.js
@@ -0,0 +1,18 @@
+﻿var FakeData = require('./fakedata');
+
+var ShelterSearch = {
+    GetShelterSearchResults: function (searchText) {
+        console.log("Search for " + searchText);
+        var results = [];
+        var fakeShelters = FakeData.GetFakeShelterData();
+        for (var i = 0; i < fakeShelters.length; i++) {
+            if (fakeShelters[i].name.indexOf(searchText) > -1) {
+                results.push(fakeShelters[i]);
+            }
+        }
+        console.log("Resurning " + results);
+        return results;
+    }
+}
+
+module.exports = ShelterSearch;
---
+++ SeeSpotHelp/SeeSpotHelp/App/sheltersearchresults.jsx
@@ -0,0 +1,22 @@
+﻿var React = require('react');
+
+var ShelterSearchResults = React.createClass({
+    loadShelter: function(shelter) {
+        // TODO(sgammon): Implement
+    },
+    generateResult: function(result) {
+        return <button onClick={this.loadShelter.bind(null, result.name)}>
+                       {result.name}
+               </button>
+    },
+    render: function() {
+        var items = this.props.results.map(this.generateResult);
+        return (
+            <div>
+          {items}
+          </div>
+        );
+    }
+});
+
+module.exports = ShelterSearchResults;
