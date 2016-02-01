8451e4019fd77e9204871ded293e41f1d4648476
Stacey Gammon <stacey@staceygammon.com>
2016-01-31 22:58:28 -0500

    Save selected volunteer group in a session state as a temporary implementation.  Will need to add login abilities to save real session state (facebook, g+ login, etc).

--- SeeSpotHelp/SeeSpotHelp/App/fakedata.js
+++ SeeSpotHelp/SeeSpotHelp/App/fakedata.js
@@ -1,26 +1,32 @@
 ﻿
 var FakeData = {
     GetFakeVolunteerGroupData: function() {
-        return [
-            {
-                "name": "Friends of Saratoga County Humane Society",
-                "shelterName": "Saratoga County Humane Society",
-                "id": "123",
-                "address": "Saratoga Springs, NY"
-            },
-            {
-                "name": "Friends of Newark AHS",
-                "shelterName": "Newark Humane Society",
-                "id": "456",
-                "address": "Newark, NJ"
-            },
-            {
-                "name": "Dog Walkers at Halfway Hounds",
-                "shelterName": "Halfway Hounds",
-                "id": "789",
-                "address": "Park Ridge, NJ"
-            }
-        ];
+        return {
+            "123": {
+                    "name": "Friends of Saratoga County Humane Society",
+                    "shelterName": "Saratoga County Humane Society",
+                    "id": "123",
+                    "address": "Saratoga Springs, NY"
+                    },
+            "456": {
+                    "name": "Friends of Newark AHS",
+                    "shelterName": "Newark Humane Society",
+                    "id": "456",
+                    "address": "Newark, NJ"
+                    },
+            "789": {
+                    "name": "Dog Walkers at Halfway Hounds",
+                    "shelterName": "Halfway Hounds",
+                    "id": "789",
+                    "address": "Park Ridge, NJ"
+                    }
+        }
+    },
+    GetFakeUser: function() {
+        return {
+            "name": "Susy Q",
+            "volunteerGroup": GetFakeVolunteerGroupData()["123"]
+        }
     }
 }
 
