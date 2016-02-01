29a7662de3411503edb9cd3fa27bc448491e5330
Stacey Gammon <stacey@staceygammon.com>
2016-02-01 15:01:42 -0500

    Add volunteer group javascript class.

---
+++ SeeSpotHelp/SeeSpotHelp/App/scripts/volunteergroup.js
@@ -0,0 +1,17 @@
+﻿// A volunteer group represents a group of volunteers at a given
+// shelter.  The most common scenario will be a one to mapping of
+// shelter to volunteer group, though it is possible for there to
+// be multiple groups linked to a single shelter. An example of this
+// is if there are two separate volunteer groups for each animal
+// type - i.e. cat volunteers and dog volunteers. Another scenario
+// is if a random person creates a volunteer group for a shelter, then
+// stops using the app.  It will just sit there unused and the real
+// volunteers will have to create a separate group.
+var VolunteerGroup = function(name, shelter, address, id) {
+    this.name = name;
+    this.shelter = shelter;
+    this.address = address;
+    this.id = id;
+}
+
+module.exports = VolunteerGroup;
