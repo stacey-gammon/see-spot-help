0ac253d002d046bb016f652c66d7a612844ea0ea
Stacey Gammon <stacey@staceygammon.com>
2016-01-31 22:59:56 -0500

    Revert "Revert "Save selected volunteer group in a session state as a temporary implementation.  Will need to add login abilities to save real session state (facebook, g+ login, etc).""
    This reverts commit b73e4633f1424b5786ee3a44d4483c8550012223.

--- SeeSpotHelp/SeeSpotHelp/App/sheltersearchresults.jsx
+++ SeeSpotHelp/SeeSpotHelp/App/sheltersearchresults.jsx
@@ -2,13 +2,9 @@
 var LinkContainer = require('react-router-bootstrap').LinkContainer;
 
 var ShelterSearchResults = React.createClass({
-    loadShelter: function(shelter) {
-        
-    },
-
     generateResult: function(result) {
         return (
-            <LinkContainer to={{ pathname: 'shelterHomePage', query: { groupName: result.name } }}>
+            <LinkContainer to={{ pathname: 'shelterHomePage', query: { groupId: result.id } }}>
                 <button className="btn btn-primary shelterResult">
                    {result.name} - {result.shelterName} - {result.address}
                 </button>
