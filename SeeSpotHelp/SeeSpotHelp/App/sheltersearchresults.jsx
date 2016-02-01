bd37d3193e5df987d234897426c8df71974c6956
Stacey Gammon <stacey@staceygammon.com>
2016-01-31 21:10:01 -0500

    Add some basic styling to shelter result buttons

--- SeeSpotHelp/SeeSpotHelp/App/sheltersearchresults.jsx
+++ SeeSpotHelp/SeeSpotHelp/App/sheltersearchresults.jsx
@@ -4,17 +4,22 @@ var ShelterSearchResults = React.createClass({
     loadShelter: function(shelter) {
         // TODO(sgammon): Implement
     },
+
     generateResult: function(result) {
-        return <button onClick={this.loadShelter.bind(null, result.name)}>
-                       {result.name}
-               </button>
+        return (
+            <button className="btn btn-primary shelterResult"
+                onClick={this.loadShelter.bind(null, result.name)}>
+               {result.name}
+            </button>
+        );
     },
+
     render: function() {
         var items = this.props.results.map(this.generateResult);
         return (
             <div>
-          {items}
-          </div>
+            {items}
+            </div>
         );
     }
 });
