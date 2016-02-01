17c3097041ccd326c679234cf883e8b4ef207570
bgaddis56 <gaddisb@gmail.com>
2016-02-01 15:38:39 -0500

    Added test page to retrieve all animals in JSON format. Also added some new code to Class Libraries

--- SeeSpotHelp/StaceyAnimals/Animals.cs
+++ SeeSpotHelp/StaceyAnimals/Animals.cs
@@ -4,9 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace StaceyAnimalsNonPortable
+namespace StaceyAnimals
 {
-    class Animals
+    public class Animals : StaceyData.BaseObject
     {
+
+ 
+        public List<StaceyAnimals.Animal> getAnimals()
+        {
+            List<StaceyAnimals.Animal> myanimals = new List<StaceyAnimals.Animal>();
+            System.Data.DataTable dt;
+            dt = Helpers.DBHelper.ExecuteQuery("select * from animals");
+            Animal tempAnimal = new Animal();
+            foreach (System.Data.DataRow element in dt.Rows)
+            {
+
+                tempAnimal.ID = (int)element["AnimalID"];
+                tempAnimal.Name = (string)element["AnimalName"];
+                tempAnimal.Description = (string)element["AnimalDescription"];
+                myanimals.Add(tempAnimal);
+            }
+
+            return myanimals;
+
+        }
     }
 }
