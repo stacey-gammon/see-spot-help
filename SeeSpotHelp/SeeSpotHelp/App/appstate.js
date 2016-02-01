7943774d7070f8d2e407cc98051ae520606878b8
Stacey Gammon <stacey@staceygammon.com>
2016-01-31 22:58:28 -0500

    Save selected volunteer group in a session state as a temporary implementation.  Will need to add login abilities to save real session state (facebook, g+ login, etc).

---
+++ SeeSpotHelp/SeeSpotHelp/App/appstate.js
@@ -0,0 +1,10 @@
+﻿// Contains the current state of the application (per user). I.E selected shelter,
+// selected animal, returned server objects.
+
+var AppState =
+{
+    user: {}
+    volunteerGroup: {}
+};
+
+module.exports = AppState;
