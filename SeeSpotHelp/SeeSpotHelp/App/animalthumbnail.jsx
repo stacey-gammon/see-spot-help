737571e44e80c57f37b3268be2fdc60d2abfa99e
Stacey Gammon <stacey@staceygammon.com>
2016-02-01 08:56:41 -0500

    Add list of animals at each shelter using fake data.  Completely unstylized.

---
+++ SeeSpotHelp/SeeSpotHelp/App/animalthumbnail.jsx
@@ -0,0 +1,15 @@
+﻿'use strict'
+
+var React = require('react');
+
+var AnimalThumbnail = React.createClass({
+    render: function () {
+        return (
+            <div>
+                {this.props.animalName}
+            </div>
+        );
+    }
+});
+
+module.exports = AnimalThumbnail;
