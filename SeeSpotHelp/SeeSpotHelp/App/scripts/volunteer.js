7a90e5f2a142459a3c92aafd25c0e07caf4f961a
Stacey Gammon <stacey@staceygammon.com>
2016-02-02 07:19:36 -0500

    Add a volunteer js class

---
+++ SeeSpotHelp/SeeSpotHelp/App/scripts/volunteer.js
@@ -0,0 +1,30 @@
+﻿// A volunteer that may or may not be part of a volunteer group. User sessions
+// managed by facebook login and authentication.
+
+var FakeData = require('./fakedata');
+
+var Volunteer = function (name, email, id) {
+    this.name = name;
+    this.email = email;
+
+    // The id is the user id given by facebook.
+    this.id = id;
+}
+
+// Returns the default volunteer group this volunteer belongs to,
+// if any. If the volunteer does not exist yet in the server db, they
+// will be inserted. Returns null if user is not attached to any
+// groups.
+Volunteer.prototype.GetDefaultVolunteerGroup = function () {
+    // TODO: implement
+    return FakeData.fakeVolunteerGroupData["123"];
+}
+
+// Updates the default volunteer group associated with the current
+// volunteer.
+Volunteer.prototype.SetDefaultVolunteerGroup = function ()
+{
+    // TODO: implement
+}
+
+module.exports = Volunteer;
