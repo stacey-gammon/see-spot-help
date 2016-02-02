8da01ced13e06b69b87d0953d35aea62a34e35e9
Stacey Gammon <stacey@staceygammon.com>
2016-02-02 13:36:30 -0500

    Created facebookuser and volunteer js classes.  Connected them to the shelter home page so the volunteers default group page loads up on default (for now using fake data, will eventually be the hook to the server side).

--- SeeSpotHelp/SeeSpotHelp/App/ui/shelterhomepage.jsx
+++ SeeSpotHelp/SeeSpotHelp/App/ui/shelterhomepage.jsx
@@ -6,20 +6,75 @@ var ShelterSearchBox = require('./sheltersearchbox');
 var ShelterInfoBox = require('./shelterinfobox');
 var ShelterActionsBox = require('./shelteractionsbox');
 var FakeData = require('../scripts/fakedata');
+var FacebookUser = require('../scripts/facebookuser');
+var Volunteer = require('../scripts/volunteer');
 
 var ShelterHomePage = React.createClass({
-    render: function () {
-        var query = this.props.location.query;
-        if (query && query.groupId) {
-            var activeGroup = FakeData.fakeVolunteerGroupData[query.groupId];
-            localStorage.setItem('activeGroup', JSON.stringify(activeGroup));
+    loadPageForVolunteer: function(facebookUser) {
+        console.log("loadPageForVolunteer");
+        if (!facebookUser) {
+            console.log("initiating new facebook user");
+            facebookUser = new FacebookUser();
         }
-        var group = JSON.parse(localStorage.getItem('activeGroup'));
-        if (group) {
-            var animals = FakeData.getFakeAnimalDataForGroup(group.id);
+
+        // TODO: Check multi-threaded access to this variable. And clean this up.
+        // It's messy, there has to be a better way using callbacks instead of
+        // a wait loop.   Probably should pass in to facebookUser a callback
+        // function so we can load the right page once the user is logged in.
+        // Make sure to avoid a flicker of "not logged in page" to "logged in page".
+        if (facebookUser.loading) {
+            console.log("Facebook user still loading, try again");
+            setTimeout(this.loadPageForVolunteer.bind(this, facebookUser), 10);
+            return;
+        }
+
+        this.setState({volunteer: facebookUser.volunteer});
+        sessionStorage.setItem("volunteer", JSON.stringify(facebookUser.volunteer));
+
+        // If there isn't yet a default group choosen for this session, seed it from
+        // server side data, whatever group the volunteer is a part of.  Searching and
+        // selecting another group will overrie the session default, so the user
+        // continues to see their last selected group, but it will not be stored on the
+        // server unless the volunteer is an actual group member.  Will have to work
+        // this use case out more.
+        if (!this.state.defaultGroup) {
+            this.setState({ "defaultGroup": facebookUser.volunteer.GetDefaultVolunteerGroup() });
+        }
+    },
+
+    getInitialState: function() {
+        console.log("ShelterHomePage::getInitialState");
+        var defaultGroup = null;
+        if (sessionStorage.getItem("defaultGroup")) {
+            console.log("defaultGroup stored as " + sessionStorage.getItem("defaultGroup"));
+            defaultGroup = JSON.parse(sessionStorage.getItem("defaultGroup"));
+        }
+
+        // TODO: We are waiting 1 second because FB SDK is loaded async, you may get a
+        // "FB is undefined" error. Will work on it.
+        setTimeout(this.loadPageForVolunteer, 1000);
+
+        return {
+            defaultGroup: defaultGroup,
+            volunteer: null
+        }
+    },
+
+    render: function () {
+        console.log("shelterhomepage::render");
+        var query = this.props.location.query;
+        var defaultGroup = this.state.defaultGroup;
+        
+        console.log("state default group? " + defaultGroup);
+        if (query && query.groupId) {
+            defaultGroup = FakeData.fakeVolunteerGroupData[query.groupId];
+            sessionStorage.setItem("defaultGroup", JSON.stringify(defaultGroup));
+        }
+        if (defaultGroup) {
+            var animals = FakeData.getFakeAnimalDataForGroup(defaultGroup.id);
             return (
                 <div>
-                    <ShelterInfoBox group={group}/>
+                    <ShelterInfoBox group={defaultGroup}/>
                     <ShelterActionsBox />
                     <hr/>
                     <AnimalList animals={animals}/>
