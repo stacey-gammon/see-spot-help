0dd758351625c98fe0b8aa26a94fb5f94a0373f1
Stacey Gammon <stacey@staceygammon.com>
2016-02-02 13:36:30 -0500

    Created facebookuser and volunteer js classes.  Connected them to the shelter home page so the volunteers default group page loads up on default (for now using fake data, will eventually be the hook to the server side).

---
+++ SeeSpotHelp/SeeSpotHelp/App/scripts/facebookuser.js
@@ -0,0 +1,32 @@
+﻿var Volunteer = require('../scripts/volunteer');
+
+var FacebookUser = function () {
+    var facebookUser = this;
+    this.loading = true;
+    this.volunteer = null;
+
+    this.loadVolunteer = function() {
+        console.log('Welcome!  Fetching your information.... ');
+        FB.api('/me', function (response) {
+            console.log('Successful login for: ' + response.name);
+            facebookUser.volunteer = new Volunteer(response.name, "fakeemail", "fakeid");
+            sessionStorage.setItem("volunteer", facebookUser.volunteer);
+            console.log("Setting loading to false");
+            facebookUser.loading = false;
+        }.bind(this));
+    }.bind(this);
+
+    this.loginCallback = function(response) {
+        if (response.status === 'connected') {
+            console.log("connected and logged in, loading volunteer");
+            facebookUser.loadVolunteer();
+        } else {
+            console.log("Setting loading to false, no login made.");
+            facebookUser.loading = false;
+        }
+    }
+
+    FB.getLoginStatus(this.loginCallback.bind(this));
+}
+
+module.exports = FacebookUser;
