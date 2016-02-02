62752f23a343dec624957b5e84958f196fe35b79
Stacey Gammon <stacey@staceygammon.com>
2016-02-01 22:51:48 -0500

    Add a facebook login button.  Works with "localhost" domain, so should work while testing in dev environment.  Will need to update when moved to a server.

--- SeeSpotHelp/SeeSpotHelp/App/ui/navbar.jsx
+++ SeeSpotHelp/SeeSpotHelp/App/ui/navbar.jsx
@@ -1,6 +1,7 @@
 ﻿var React = require('react');
 var ReactRouterBootstrap = require('react-router-bootstrap');
 var ReactBootstrap = require('react-bootstrap');
+var FacebookLogin = require('./facebooklogin');
 
 var Navbar = ReactBootstrap.Navbar;
 var Nav = ReactBootstrap.Nav;
@@ -22,6 +23,11 @@ var MyNavBar = React.createClass({
                         <span className="glyphicon glyphicon-search" />
                     </NavItem>
                     </LinkContainer>
+                    </Nav>
+                <Nav className="nav navbar-nav navbar-right navbar-right-custom ">
+                    <NavItem>
+                        <FacebookLogin />
+                    </NavItem>
                 </Nav>
             </Navbar>
         );
