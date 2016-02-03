d3f4b0a2de981f19521af463842f0a270cc6f205
Stacey Gammon <stacey@staceygammon.com>
2016-02-02 21:30:27 -0500

    Move facebook login code to home and pass user around as a prop or state variable (depending on call - dynamically generated location changes seem to only allow state, while static ones are okay with prop).

--- SeeSpotHelp/SeeSpotHelp/App/scripts/volunteergroup.js
+++ SeeSpotHelp/SeeSpotHelp/App/scripts/volunteergroup.js
@@ -7,11 +7,44 @@
 // is if a random person creates a volunteer group for a shelter, then
 // stops using the app.  It will just sit there unused and the real
 // volunteers will have to create a separate group.
+
 var VolunteerGroup = function(name, shelter, address, id) {
     this.name = name;
     this.shelter = shelter;
     this.address = address;
     this.id = id;
+
+    // Mapping of user id to permission enum, one entry per
+    // member in the volunteer group.
+    this.userPermissionsMap = {};
+};
+
+VolunteerGroup.PermissionsEnum = Object.freeze(
+    {
+        MEMBER: 0,
+        ADMIN: 1,
+        NONMEMBER: 2
+    }
+);
+
+VolunteerGroup.prototype.GetUserPermissions = function (userId) {
+    return this.PermissionsEnum.MEMBER;
+
+    // TODO: Use once loadVolunteerGroup is working.
+    var permissions = userPermissionsMap[userId];
+    if (!permissions) return this.PermissionsEnum.NONMEMBER;
+    return permissions;
 }
+
+VolunteerGroup.prototype.SaveVolunteerGroup = function() {
+    // TODO: Implement and hook into database.
+}
+
+// Returns a volunteer group object for the given id.  null if
+// no volunteer group with that id exists.
+VolunteerGroup.loadVolunteerGroup = function (groupId) {
+    // TODO: Implement and hook into database.
+    return FakeData.fakeVolunteerGroupData[groupId];
+};
 
 module.exports = VolunteerGroup;
