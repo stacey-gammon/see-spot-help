c2ee0f643820dc509cbaa76c6abffbdbc964f8f0
Stacey Gammon <stacey@staceygammon.com>
2016-02-02 22:52:03 -0500

    Add "add new shelter" button up and have it respond to user logged in status.

--- SeeSpotHelp/SeeSpotHelp/App/ui/sheltersearchpage.jsx
+++ SeeSpotHelp/SeeSpotHelp/App/ui/sheltersearchpage.jsx
@@ -1,16 +1,23 @@
-﻿'use strict'
-var React = require('react');
-var ShelterSearchBox = require('./sheltersearchbox');
+﻿"use strict";
+
+var React = require("react");
+var ShelterSearchBox = require("./sheltersearchbox");
 
 var ShelterSearchPage = React.createClass({
     render: function() {
+        console.log("ShelterSearchPage:render");
+        var user = this.props.user;
+        if (!user && this.props.location.state && this.props.location.state.user) {
+            console.log("User set in state, loading..");
+            user = this.props.location.state.user;
+        }
         return (
-          <div>
-            <ShelterSearchBox user={this.props.user}/>
+            <div>
+            <ShelterSearchBox user={user}/>
             {this.props.children}
-          </div>
-      );
-}
+            </div>
+        );
+    }
 });
 
 module.exports = ShelterSearchPage;
