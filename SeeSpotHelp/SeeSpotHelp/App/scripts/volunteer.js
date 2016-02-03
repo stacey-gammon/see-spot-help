eb7b00e4c1c8799bbcc9de105f73e9c48f0175d7
bgaddis56 <gaddisb@gmail.com>
2016-02-03 14:44:34 -0500

    added web service for retrieving volunteer and added volunteer project with volunteer object.

--- SeeSpotHelp/SeeSpotHelp/App/scripts/volunteer.js
+++ SeeSpotHelp/SeeSpotHelp/App/scripts/volunteer.js
@@ -20,12 +20,41 @@ function Volunteer(volunteer) {
 // database.  If no such volunteer exists, AddNewVolunteer
 // will be called with some basic defaults supplied by
 // facebook.
-Volunteer.prototype.LoadVolunteer = function () {
+function LoadVolunteer(anID) {
     // TODO: Implement
+    //$.ajax({
+    //    url: "BHGtest.aspx",
+    //    dataType: "json",
+    //    success: function (data) {
+    //        alert(data);
+    //    },
+
+    //    error: function () { alert("Ajax Error"); }
+    //});
+
+    $.ajax({
+        type: "POST",
+        url: "WebServices/volunteerServices.asmx/getVolunteer",
+        data: '{anID: ' + anID + '}',
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (data) {
+            var volLit = JSON.parse(data.d);
+            var newVol = new Volunteer();
+            for (var prop in volLit)
+                newVol[prop] = volLit[prop];
+            return newVol;
+        },
+
+        error: function (ts) { alert(ts.responseText); }
+    });
+
+
 }
 
 Volunteer.prototype.AddNewVolunteer = function() {
     // TODO: Implement
+
 }
 
 // Returns the default volunteer group this volunteer belongs to,
