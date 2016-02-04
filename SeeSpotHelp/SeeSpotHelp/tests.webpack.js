3d41615336bd809ef2f796a396d69f1c306fde28
Stacey Gammon <stacey@staceygammon.com>
2016-02-04 13:28:11 -0500

    Add InputField and InputFieldValidation js classes and set up use for add new shelter page.  Add basic add new shelter test.

--- SeeSpotHelp/SeeSpotHelp/tests.webpack.js
+++ SeeSpotHelp/SeeSpotHelp/tests.webpack.js
@@ -1,3 +1,4 @@
 ﻿// tests.webpack.js
-var context = require.context('./App', true, /-test\.jsx?$/);
+//var context = require.context('./App', true, /-test\.jsx?$/);
+var context = require.context('./App/tests', true, /addnewshelterpage-test\.jsx?$/);
 context.keys().forEach(context);
