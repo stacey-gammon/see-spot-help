65d3646419cfb461051ebe0c358a0c41165c20a9
Stacey Gammon <stacey@staceygammon.com>
2016-02-03 19:30:22 -0500

    Add request to join button for non members with tests (no functionality yet)

--- SeeSpotHelp/SeeSpotHelp/App/tests/shelteractionsbox-test.jsx
+++ SeeSpotHelp/SeeSpotHelp/App/tests/shelteractionsbox-test.jsx
@@ -8,7 +8,7 @@ var expect = require("expect"),
 var d3 = require("d3");
 
 describe("ShelterActionsBox", function () {
-    it("Contains leave button for members", function () {
+    it("LeaveButtonForMember", function () {
         var volunteer = new Volunteer("Sally", "sally@sally.com", "115");
         var group = new VolunteerGroup("Group name", "shelter name", "address", "25");
         group.userPermissionsMap["115"] = VolunteerGroup.PermissionsEnum.MEMBER;
@@ -21,7 +21,7 @@ describe("ShelterActionsBox", function () {
             shelterActionsBox, "leaveShelterButton");
     });
 
-    it("No leave button for nonmembers", function () {
+    it("NoLeaveButtonForNonMember", function () {
         var volunteer = new Volunteer("Sally", "sally@sally.com", "115");
         var group = new VolunteerGroup("Group name", "shelter name", "address", "25");
 
@@ -31,6 +31,32 @@ describe("ShelterActionsBox", function () {
 
         var leaveButtons = ReactTestUtils.scryRenderedDOMComponentsWithClass(
             shelterActionsBox, "leaveShelterButton");
+        expect(leaveButtons.length).toEqual(0);
+    });
+
+    it("RequestJoinButtonForNonMember", function () {
+        var volunteer = new Volunteer("Sally", "sally@sally.com", "115");
+        var group = new VolunteerGroup("Group name", "shelter name", "address", "25");
+
+        var shelterActionsBox = ReactTestUtils.renderIntoDocument(
+            <ShelterActionsBox user={volunteer} group={group}/>
+        );
+
+        ReactTestUtils.findRenderedDOMComponentWithClass(
+            shelterActionsBox, "requestToJoinButton");
+    });
+
+    it("NoRequestJoinButtonForMember", function () {
+        var volunteer = new Volunteer("Sally", "sally@sally.com", "115");
+        var group = new VolunteerGroup("Group name", "shelter name", "address", "25");
+        group.userPermissionsMap["115"] = VolunteerGroup.PermissionsEnum.MEMBER;
+
+        var shelterActionsBox = ReactTestUtils.renderIntoDocument(
+            <ShelterActionsBox user={volunteer} group={group}/>
+        );
+
+        var leaveButtons = ReactTestUtils.scryRenderedDOMComponentsWithClass(
+            shelterActionsBox, "requestToJoinButton");
         expect(leaveButtons.length).toEqual(0);
     });
 });
