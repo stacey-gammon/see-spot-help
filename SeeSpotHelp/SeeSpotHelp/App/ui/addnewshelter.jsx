b3f4516948467bfea2e0171d9f139c3bd674b5ac
Stacey Gammon <stacey@staceygammon.com>
2016-02-03 22:08:53 -0500

    Start to fill out Add new shelter page with inputs.  Only partially there.

--- SeeSpotHelp/SeeSpotHelp/App/ui/addnewshelter.jsx
+++ SeeSpotHelp/SeeSpotHelp/App/ui/addnewshelter.jsx
@@ -1,17 +1,67 @@
 ﻿"use strict"
 
 var React = require("react");
+var ConstStrings = require("../scripts/conststrings");
+var VolunteerGroup = require("../scripts/volunteergroup");
+
+var STATES = [
+    "AL", "AK", "AS", "AZ", "AR", "CA", "CO", "CT", "DE", "DC", "FL", "GA", "HI",
+    "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS",
+    "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR",
+    "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"
+];
 
 var AddNewShelter = React.createClass({
+    getInitialState: function() {
+        return {
+            errorMessage: null,
+            fields: ["groupName", "shelterName", "address", "city", "state", "zipCode"]
+        };
+    },
+
+    addNewVolunteerGroup: function() {
+        var values = {};
+        var errorsFound = false;
+        for (var i = 0; i < this.state.fields.length; i++) {
+            var field = this.state.fields[i];
+            if (!this.refs[field].value) {
+                this.setState({ errorMessage: "Please fill in all fields!" });
+                errorsFound = true;
+            } else {
+                values[field] = this.refs[field].value;
+            }
+        }
+        if (!errorsFound) {
+            this.setState({ errorMessage: null });
+        }
+        var group = new VolunteerGroup(values["groupName"],
+                                       values["shelterName"],
+                                       values["address"]);
+        group.addNewVolunteerGroup();
+    },
+
+    createInputField: function (name) {
+        return (
+            <div className="input-group">
+                <span className="input-group-addon">{ConstStrings[name]}</span>
+                <input type="text" ref={name} className="form-control"/>
+            </div>
+        );
+    },
+
     render: function () {
-        var user = this.props.user;
-        if (!user && this.props.location.state && this.props.location.state.user) {
-            user = this.props.location.state.user;
+        var user = this.props.location.state;
+        var inputFields = [];
+        for (var i = 0; i < this.state.fields.length; i++) {
+            var field = this.state.fields[i];
+            inputFields.push(this.createInputField(field));
         }
         if (user) {
             return (
                 <div>
-                    <h1>Add New Group</h1>
+                    {this.state.errorMessage}
+                    {inputFields}
+                    <button className="btn btn-primary" onClick={this.addNewVolunteerGroup}>Add Group</button>
                 </div>
             );
         } else {
