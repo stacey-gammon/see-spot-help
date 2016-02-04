b5ecc5b863e9a1bd8583ac2c5873674506f615a4
Stacey Gammon <stacey@staceygammon.com>
2016-02-03 22:08:53 -0500

    Start to fill out Add new shelter page with inputs.  Only partially there.

--- SeeSpotHelp/SeeSpotHelp/App/scripts/conststrings.js
+++ SeeSpotHelp/SeeSpotHelp/App/scripts/conststrings.js
@@ -2,8 +2,13 @@
 var ConstStrings = {
     RequestToJoin: "Request to join",
     JoinRequestPending: "Request pending",
-    LeaveGroup: "Leave group"
+    LeaveGroup: "Leave group",
+    groupName: "Group Name",
+    shelterName: "Shelter",
+    address: "Address",
+    city: "City",
+    state: "State",
+    zipCode: "zipCode"
 };
 
 module.exports = ConstStrings;
-
