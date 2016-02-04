be707358c6e4c724117312213c1a5d1d0d7565be
Stacey Gammon <stacey@staceygammon.com>
2016-02-03 20:55:19 -0500

    Hopefully fixed the issue with FB loading async and sometimes not completing before being called

--- SeeSpotHelp/SeeSpotHelp/App/ui/shelteractionsbox.jsx
+++ SeeSpotHelp/SeeSpotHelp/App/ui/shelteractionsbox.jsx
@@ -3,6 +3,7 @@
 var React = require("react");
 var VolunteerGroup = require("../scripts/volunteergroup");
 var Volunteer = require("../scripts/volunteer");
+var ConstStrings = require("../scripts/conststrings");
 
 var LeaveShelterButton = React.createClass({
     alertNotImplemented: function () {
@@ -10,13 +11,15 @@ var LeaveShelterButton = React.createClass({
     },
     render: function () {
         console.log("LeaveShelterButton:render, permissions = " + this.props.permissions);
-        if (this.props.permissions == VolunteerGroup.PermissionsEnum.NONMEMBER)
+        if (!this.props.user ||
+            this.props.permissions == VolunteerGroup.PermissionsEnum.NONMEMBER) {
             return null;
+        }
         return (
             <div>
                 <button className="btn btn-warning leaveShelterButton"
                         onClick={this.alertNotImplemented}>
-                    Leave Group
+                    {ConstStrings.LeaveGroup}
                 </button>
             </div>
         );
@@ -24,18 +27,35 @@ var LeaveShelterButton = React.createClass({
 });
 
 var RequestToJoinButton = React.createClass({
-    alertNotImplemented: function () {
-        alert('Sorry, that functionality is not implemented yet!');
+    requestToJoin: function () {
+        if (!this.props.group || !this.props.user) {
+            throw "Attempting to join group when user or group is undefined or null";
+        }
+        var group = VolunteerGroup.castObject(this.props.group);
+        var user = Volunteer.castObject(this.props.user);
+        group.requestToJoin(user);
+        this.refs.requestToJoinButton.disabled = true;
+        this.refs.requestToJoinButton.innerHTML = ConstStrings.JoinRequestPending;
     },
+
     render: function () {
         console.log("RequestToJoinButton:render, permissions = " + this.props.permissions);
-        if (this.props.permissions != VolunteerGroup.PermissionsEnum.NONMEMBER)
+        if (!this.props.user) return null;
+
+        var pending = this.props.permissions == VolunteerGroup.PermissionsEnum.PENDINGMEMBERSHIP;
+
+        if (this.props.permissions != VolunteerGroup.PermissionsEnum.NONMEMBER &&
+            !pending) {
             return null;
+        }
+        var text = pending ? ConstStrings.JoinRequestPending : ConstStrings.RequestToJoin;
         return (
             <div>
                 <button className="btn btn-warning requestToJoinButton"
-                        onClick={this.alertNotImplemented}>
-                    Request to Join
+                        ref="requestToJoinButton"
+                        disabled={pending}
+                        onClick={this.requestToJoin}>
+                    {text}
                 </button>
             </div>
         );
@@ -45,13 +65,13 @@ var RequestToJoinButton = React.createClass({
 var ShelterActionsBox = React.createClass({
     render: function () {
         console.log("ShelterActionsBox:render:");
-        var volunteer = this.props.user ? Volunteer.castObject(this.props.user) : null;
+        var user = this.props.user ? Volunteer.castObject(this.props.user) : null;
         var group = this.props.group ? VolunteerGroup.castObject(this.props.group) : null;
-        var permissions = group.getUserPermissions(volunteer.id);
+        var permissions = user && group ? group.getUserPermissions(user.id) : null;
         return (
             <div>
-                <LeaveShelterButton permissions={permissions}/>
-                <RequestToJoinButton permissions={permissions} />
+                <LeaveShelterButton permissions={permissions} user={user}/>
+                <RequestToJoinButton permissions={permissions} user={user} group={group} />
             </div>
         );
     }
