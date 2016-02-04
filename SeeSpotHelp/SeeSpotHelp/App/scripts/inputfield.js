20dd40eec8c74553f398058ebdc80fac8ef54389
Stacey Gammon <stacey@staceygammon.com>
2016-02-04 13:28:11 -0500

    Add InputField and InputFieldValidation js classes and set up use for add new shelter page.  Add basic add new shelter test.

---
+++ SeeSpotHelp/SeeSpotHelp/App/scripts/inputfield.js
@@ -0,0 +1,58 @@
+﻿// Represents an input form field.
+// @param validations {inputfieldvalidations[]} an array list of input field validations
+// that this field should run during the validate call.
+var InputField = function (validations) {
+    this.hasError = false;
+    this.validated = false;
+    this.errorMessage = "";
+    this.value = "";
+    this.validations = validations;
+    this.ref = "";
+};
+
+// Loops through all validations this form field has. The current
+// object will be updated depending on success or failure.
+InputField.prototype.validate = function() {
+    for (var i = 0; i < validations.length; i++) {
+        validations[i].validate(this);
+        // Stop after the first error is encountered.
+        if (this.hasError) return;
+    }
+};
+
+// If the field has an error on it, will return an error icon span.
+// If the field was validated successfully, will return a span with
+// a success mark.  Otherwise returns null.  Note the dom element
+// creation is different from the jsx files because this is a plain
+// old js file.
+InputField.prototype.getValidationSpan = function() {
+    if (this.hasError) {
+        return React.createElement(
+            span, { class: "glyphicon glyphicon-remove form-control-feedback" });
+    } else if (this.validated) {
+        return React.createElement(
+            span, { class: "glyphicon glyphicon-ok form-control-feedback" });
+    } else {
+        return null;
+    }
+};
+
+// If an error occured, returns a dom label with the message, otherwise returns
+// null.
+InputField.prototype.getErrorLabel = function() {
+    if (this.hasError) {
+        return React.createElement(label, { class: "control-label", for: this.ref }, this.errorMessage);
+    } else {
+        return null;
+    }
+};
+
+// Returns the classname that should be used on the form-group div that is
+// housing this input field.
+InputField.prototype.getFormGroupClassName = function() {
+    if (this.hasError) return "form-group has-error";
+    if (this.validated) return "form-group has-success";
+    return "";
+};
+
+module.exports = InputField;
---
+++ SeeSpotHelp/SeeSpotHelp/App/tests/addnewshelterpage-test.jsx
@@ -0,0 +1,29 @@
+﻿var React = require("react");
+var ReactTestUtils = require("react-addons-test-utils");
+var expect = require("expect"),
+    Volunteer = require("../scripts/volunteer"),
+    VolunteerGroup = require("../scripts/volunteergroup"),
+    AddNewShelter = require("../ui/addnewshelter.jsx");
+
+var d3 = require("d3");
+
+describe("AddNewShelter", function () {
+    it("AddNewShelterLoadsInputFields", function () {
+        var user = new Volunteer("john", "doe", "123");
+        var addNewShelter = ReactTestUtils.renderIntoDocument(
+            <AddNewShelter user={user}/>
+        );
+        ReactTestUtils.findRenderedDOMComponentWithClass(
+            addNewShelter, "groupName");
+        ReactTestUtils.findRenderedDOMComponentWithClass(
+            addNewShelter, "shelterName");
+        ReactTestUtils.findRenderedDOMComponentWithClass(
+            addNewShelter, "address");
+        ReactTestUtils.findRenderedDOMComponentWithClass(
+            addNewShelter, "city");
+        ReactTestUtils.findRenderedDOMComponentWithClass(
+            addNewShelter, "state");
+        ReactTestUtils.findRenderedDOMComponentWithClass(
+            addNewShelter, "zipCode");
+    });
+});
