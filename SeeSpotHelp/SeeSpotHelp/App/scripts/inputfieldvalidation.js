c3f76f04ccf9b1dcabb5af0e6cc9626a11727707
Stacey Gammon <stacey@staceygammon.com>
2016-02-04 20:08:07 -0500

    Complete verification of input fields not being empty when adding a new volunteer group and add mocked out hook into server side.  Add more tests.

--- SeeSpotHelp/SeeSpotHelp/App/scripts/inputfieldvalidation.js
+++ SeeSpotHelp/SeeSpotHelp/App/scripts/inputfieldvalidation.js
@@ -5,12 +5,14 @@ var InputFieldValidation = function() {};
 // Makes sure the given input field is does not contain an empty value.
 // Updates inputField based on success or failure.
 // @param inputField {InputField} - the field to validate.
-InputFieldValidation.validateNotEmpty = function(inputField) {
+InputFieldValidation.validateNotEmpty = function (inputField) {
     if (inputField.value == null || inputField.value.trim() == "") {
         inputField.hasError = true;
         inputField.errorMessage = "Field must not be empty.";
     } else {
         inputField.validated = true;
+        inputField.hasError = false;
+        inputField.errorMessage = "";
     }
 };
 
