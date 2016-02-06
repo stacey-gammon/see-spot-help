43c69cdcaaaee32a486958d57d213b554bce84c0
Stacey Gammon <stacey@staceygammon.com>
2016-02-06 11:03:00 -0500

    Modified ajax callback function.

--- SeeSpotHelp/SeeSpotHelp/App/scripts/AJAXServices.js
+++ SeeSpotHelp/SeeSpotHelp/App/scripts/AJAXServices.js
@@ -1,12 +1,18 @@
 ﻿// A helpful class filled with static functions for validating various
 // input fields.
-var AJAXServices = function () {};
+var AJAXServices = function () { };
+
 //Used to send a JSON based Web Service Request to the server
 ///*
 //*  A JSON web service MUST have the <ScriptService> attribute, and any methods called must have a <ScriptMethod> attribute.
 //*/
 //Note: The __type property must be the first JSON property of an object to ensure proper serialization/deserialization
-AJAXServices.CallJSONService = function(callbackURI, methodName, params, ReceiveServerData, ProcessError) {
+AJAXServices.CallJSONService = function (callbackURI,
+                                         methodName,
+                                         params,
+                                         ReceiveServerData,
+                                         ProcessError) {
+    console.log("AJAXServices::CallJSONService");
     $.ajax({
         type: 'POST',
         contentType: 'application/json',
@@ -17,7 +23,6 @@ AJAXServices.CallJSONService = function(callbackURI, methodName, params, Receive
     }).
         done(ReceiveServerData).
         fail(ProcessError);   // response data contains the javascript object parsed from the JSON data.
-}
-
+};
 
 module.exports = AJAXServices;
