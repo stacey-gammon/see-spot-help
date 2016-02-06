84e00464c6655a957ac35ca748b9a7d7f9489b02
Stacey Gammon <stacey@staceygammon.com>
2016-02-06 11:53:38 -0500

    Make AjaxServices a non static class so we can separate success/failure callbacks from ajax with issues from our outer layer of success, failure callbacks

--- SeeSpotHelp/SeeSpotHelp/App/scripts/AJAXServices.js
+++ SeeSpotHelp/SeeSpotHelp/App/scripts/AJAXServices.js
@@ -1,17 +1,18 @@
-﻿// A helpful class filled with static functions for validating various
+﻿// A helpful class filled with functions for validating various
 // input fields.
-var AJAXServices = function () { };
+var AJAXServices = function (successCallback, failureCallback) {
+    this.successCallback = successCallback;
+    this.failureCallback = failureCallback;
+};
 
 //Used to send a JSON based Web Service Request to the server
 ///*
 //*  A JSON web service MUST have the <ScriptService> attribute, and any methods called must have a <ScriptMethod> attribute.
 //*/
 //Note: The __type property must be the first JSON property of an object to ensure proper serialization/deserialization
-AJAXServices.CallJSONService = function (callbackURI,
-                                         methodName,
-                                         params,
-                                         ReceiveServerData,
-                                         ProcessError) {
+AJAXServices.prototype.CallJSONService = function (callbackURI,
+                                                   methodName,
+                                                   params) {
     console.log("AJAXServices::CallJSONService");
     $.ajax({
         type: 'POST',
@@ -21,8 +22,22 @@ AJAXServices.CallJSONService = function (callbackURI,
         processData: false,
         data: JSON.stringify($(params)[0]) // params need to be in a single json object.  Arrays are right out.
     }).
-        done(ReceiveServerData).
-        fail(ProcessError);   // response data contains the javascript object parsed from the JSON data.
+        done(function (response) {
+            this.onSuccess(response);
+        }.bind(this)).
+        fail(function (response) {
+            this.onFailure(response);
+        }.bind(this));   // response data contains the javascript object parsed from the JSON data.
+};
+
+AJAXServices.prototype.onSuccess = function (response) {
+    console.log("AJAXServices::OnSuccess");
+    this.successCallback(response);
+};
+
+AJAXServices.prototype.onFailure = function (response) {
+    console.log("AJAXServices::OnFailure");
+    this.failureCallback(response);
 };
 
 module.exports = AJAXServices;
