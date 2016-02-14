a93f1c4f22cf663a6219f86af98c17714c6e9559
Stacey Gammon <stacey@staceygammon.com>
2016-02-14 08:40:06 -0500

    Start to use Flux architecture to handle login/logout and global user instance.  Things now need some major clean up but this is a start.

---
+++ SeeSpotHelp/SeeSpotHelp/App/dispatcher/dispatcher.js
@@ -0,0 +1,12 @@
+﻿"use strict"
+
+var Dispatcher = require("flux").Dispatcher;
+
+class AppDispatcher extends Dispatcher {
+    constructor() {
+        super();
+        console.log("AppDispatcher:constructor");
+    }
+};
+
+module.exports = new AppDispatcher();
