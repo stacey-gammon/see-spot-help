d84c9d43a1087b9edb9c41bcf590a1210bad3f93
Stacey Gammon <stacey@staceygammon.com>
2016-02-14 08:40:06 -0500

    Start to use Flux architecture to handle login/logout and global user instance.  Things now need some major clean up but this is a start.

--- SeeSpotHelp/SeeSpotHelp/App/ui/sheltersearchresults.jsx
+++ SeeSpotHelp/SeeSpotHelp/App/ui/sheltersearchresults.jsx
@@ -1,8 +1,28 @@
 ﻿var React = require('react');
 var Router = require('react-router');
 var LinkContainer = require('react-router-bootstrap').LinkContainer;
+var LoginStore = require("../stores/loginstore");
 
 var ShelterSearchResults = React.createClass({
+    getInitialState: function () {
+        return {
+            user: LoginStore.user
+        }
+    },
+    componentDidMount: function () {
+        LoginStore.addChangeListener(this.onChange);
+    },
+
+    componentWillUnmount: function () {
+        LoginStore.removeChangeListener(this.onChange);
+    },
+
+    onChange: function () {
+        this.setState(
+            {
+                user: LoginStore.user
+            });
+    },
     contextTypes: {
         router: React.PropTypes.object.isRequired
     },
@@ -11,7 +31,7 @@ var ShelterSearchResults = React.createClass({
         sessionStorage.setItem("defaultGroup", JSON.stringify(group));
         this.context.router.push({ pathname: "/shelterHomePage",
                                    query: { groupId: group.id },
-                                   state: { defaultGroup: group, user: this.props.user } });
+                                   state: { defaultGroup: group, user: this.state.user } });
     },
     generateResult: function(result) {
         return (
