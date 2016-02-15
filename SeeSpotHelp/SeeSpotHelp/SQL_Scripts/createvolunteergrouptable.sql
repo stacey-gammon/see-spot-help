4212c8cf31c1d0e54a8af212e9aabcac47e66948
Stacey Gammon <stacey@staceygammon.com>
2016-02-14 20:57:01 -0500

    Start to hook server side volunteer to volunteer group logic in place.

--- SeeSpotHelp/SeeSpotHelp/SQL_Scripts/createvolunteergrouptable.sql
+++ SeeSpotHelp/SeeSpotHelp/SQL_Scripts/createvolunteergrouptable.sql
@@ -1,1 +1,9 @@
-﻿
+﻿CREATE TABLE dbo.VolunteerGroup
+   (Id varchar(25) NOT NULL,
+    Name varchar(100) NOT NULL,
+    ShelterName varchar(100) NOT NULL,
+    ShelterAddress varchar(100),
+    ShelterCity varchar(100),
+    ShelterState varchar(100),
+    ShelterZip varchar(100))
+GO
