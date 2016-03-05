dbd4fafed3e8dd454285e10ab0dcdca9a1bf26a3
Stacey Gammon <stacey@staceygammon.com>
2016-03-05 09:50:00 -0500

    add files

---
+++ SeeSpotHelp/SeeSpotHelp/App/ui/person/useractivitytab.jsx
@@ -0,0 +1,86 @@
+"use strict"
+
+var React = require("react");
+var Link = require("react-router").Link;
+var AnimalList = require("../animal/animallist");
+var SearchBox = require("../searchbox");
+var GroupInfoBox = require("../group/groupinfobox");
+var GroupActionsBox = require("../group/groupactionsbox");
+var FakeData = require("../../core/fakedata");
+var Volunteer = require("../../core/volunteer");
+var VolunteerGroup = require("../../core/volunteergroup");
+var LoginStore = require("../../stores/loginstore");
+var GroupStore = require("../../stores/groupstore");
+var AnimalStore = require("../../stores/animalstore");
+var AnimalActivityStore = require("../../stores/animalactivitystore");
+var AJAXServices = require("../../core/AJAXServices");
+var AddAdoptableButton = require("../animal/addanimalbutton");
+var AnimalActivityItem = require("../animal/animalactivityitem");
+
+var UserActivityTab = React.createClass({
+    getInitialState: function () {
+        var group = this.props.location &&
+                    this.props.location.state &&
+                    this.props.location.state.group ||
+                    this.props.group;
+        return {
+            user: LoginStore.getUser(),
+            group: group
+        }
+    },
+
+    componentWillReceiveProps: function(nextProps) {
+        this.setState({
+            group: nextProps.group
+        });
+    },
+
+    componentDidMount: function () {
+        AnimalActivityStore.addChangeListener(this.onChange);
+        LoginStore.addChangeListener(this.onChange);
+        GroupStore.addChangeListener(this.onChange);
+    },
+
+    componentWillMount: function () {
+    },
+
+    componentWillUnmount: function () {
+        AnimalActivityStore.removeChangeListener(this.onChange);
+        LoginStore.removeChangeListener(this.onChange);
+        GroupStore.removeChangeListener(this.onChange);
+    },
+
+    onChange: function () {
+        this.setState(
+            {
+                user: LoginStore.user,
+                group: this.state.group ? GroupStore.getGroupById(this.state.group.id) : null
+            });
+    },
+
+    generateActivity: function (activity) {
+        var group = GroupStore.getGroupById(activity.groupId);
+        return (
+            <AnimalActivityItem activity={activity}
+                                group={group}
+                                showAnimalInfo="true"/>
+        );
+    },
+
+    render: function () {
+        var notes =
+            AnimalActivityStore.getActivityByUserId(this.state.user.id);
+
+        var displayNotes = [];
+        for (var i = 0; i < notes.length; i++) {
+            displayNotes.push(this.generateActivity(notes[i]));
+        }
+        return (
+            <div className="list-group">
+                {displayNotes}
+            </div>
+        );
+    }
+});
+
+module.exports = UserActivityTab;
---
+++ SeeSpotHelp/SeeSpotHelp/App/ui/person/usergroupstab.jsx
@@ -0,0 +1,95 @@
+"use strict"
+
+var React = require("react");
+var Link = require("react-router").Link;
+var AnimalList = require("../animal/animallist");
+var SearchBox = require("../searchbox");
+var GroupInfoBox = require("../group/groupinfobox");
+var GroupActionsBox = require("../group/groupactionsbox");
+var FakeData = require("../../core/fakedata");
+var Volunteer = require("../../core/volunteer");
+var VolunteerGroup = require("../../core/volunteergroup");
+var LoginStore = require("../../stores/loginstore");
+var GroupStore = require("../../stores/groupstore");
+var AnimalActivityStore = require("../../stores/animalactivitystore");
+var AJAXServices = require("../../core/AJAXServices");
+var AddAdoptableButton = require("../animal/addanimalbutton");
+var AnimalActivityItem = require("../animal/animalactivityitem");
+var GroupListItem = require("../group/grouplistitem");
+
+var UserGroupsTab = React.createClass({
+    getInitialState: function () {
+        var user = LoginStore.getUser();
+        var groups = user ? GroupStore.getUsersMemberGroups(user) : [];
+        return {
+            user: user,
+            groups: groups
+        }
+    },
+
+    onChange: function () {
+        this.setState(
+            {
+                user: LoginStore.user,
+                groups: GroupStore.getUsersMemberGroups(LoginStore.user)
+            });
+    },
+
+    componentDidMount: function () {
+        LoginStore.addChangeListener(this.onChange);
+        GroupStore.addChangeListener(this.onChange);
+    },
+
+    componentWillUnmount: function () {
+        LoginStore.removeChangeListener(this.onChange);
+        GroupStore.removeChangeListener(this.onChange);
+    },
+
+    getGroupElement: function(group) {
+        console.log("ProfilePage:GetGroupElement");
+        return (
+            <GroupListItem user={this.state.user} group={group}/>
+        );
+    },
+
+    getGroups: function() {
+        console.log("ProfilePage:getGroups");
+        if (this.state.groups.length == 0) {
+            return (
+                <div>
+                    <h1>You are not part of any volunteer groups.  To get started&nbsp;
+                    <Link to="searchPage">search</Link>
+                    &nbsp;for a group to join, or&nbsp;
+                    <Link to="addNewGroup">add</Link> a new one.
+                    </h1>
+                </div>
+            );
+        } else if (this.state.groups.length) {
+            var groups = this.state.groups.map(this.getGroupElement);
+            return (
+                <div>
+                    {groups}
+                    <div>
+                        <h1>
+                        <Link to="searchPage">Search</Link>
+                            &nbsp;for a new group to join, or&nbsp;
+                        <Link to="addNewGroup">add</Link> your own!
+                        </h1>
+                    </div>
+                </div>
+            );
+        } else {
+            return <div></div>
+        }
+    },
+
+    render: function () {
+        return (
+            <div className="list-group">
+                {this.getGroups()}
+            </div>
+        );
+    }
+});
+
+module.exports = UserGroupsTab;
