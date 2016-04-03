0b0d46018d27bec0b934cafea731e37e0c26d07d
Stacey Gammon <stacey@staceygammon.com>
2016-04-02 21:00:36 -0400

    fix disabled beta email address input

--- SeeSpotHelp/SeeSpotHelp/App/ui/privatebetapage.jsx
+++ SeeSpotHelp/SeeSpotHelp/App/ui/privatebetapage.jsx
@@ -19,11 +19,12 @@ var PrivateBetaPage = React.createClass({
 		return (
 			<div>
 				<div id="mc_embed_signup">
-					<form action="//staceygammonpetphotography.us4.list-manage.com/subscribe/post?u=4a8ec322013131b3a2b0b83d1&amp;id=b567aa5e0c" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" novalidate>
+
+					<form action="//staceygammonpetphotography.us4.list-manage.com/subscribe/post?u=4a8ec322013131b3a2b0b83d1&amp;id=b567aa5e0c" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" noValidate>
 						<div id="mc_embed_signup_scroll">
-							<input type="email" value="" width="100%" name="EMAIL" className="email" id="mce-EMAIL" placeholder="email address" required/>
+							<input type="text" width="100%" name="EMAIL" className="email" id="mce-EMAIL" placeholder="email address" required/>
 							<div style={{position: 'absolute', left: '-5000px'}} aria-hidden="true">
-								<input type="text" name="b_4a8ec322013131b3a2b0b83d1_b567aa5e0c" tabindex="-1" value=""/></div>
+								<input type="text" name="b_4a8ec322013131b3a2b0b83d1_b567aa5e0c" tabIndex="-1" value=""/></div>
 							<div className="clear">
 								<input type="submit" value="Sign Up" name="subscribe" id="mc-embedded-subscribe" className="button"/>
 							</div>
