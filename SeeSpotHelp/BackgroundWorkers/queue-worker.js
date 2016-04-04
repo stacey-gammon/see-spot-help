fff97fcb9fe590c8f48c93b43741186974beb224
Stacey Gammon <stacey@staceygammon.com>
2016-04-04 07:35:12 -0400

    give email a template look and add unsub links to try to avoid spam

--- SeeSpotHelp/BackgroundWorkers/queue-worker.js
+++ SeeSpotHelp/BackgroundWorkers/queue-worker.js
@@ -10,6 +10,38 @@ var firebaseUrl = 'https://shining-torch-1432.firebaseio.com';
 
 var queueRef = new Firebase(firebaseUrl + '/emails');
 
+var htmlMessageCode = '' +
+'<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">' +
+'<html xmlns="http://www.w3.org/1999/xhtml">' +
+ '<head>' +
+ ' <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />' +
+ ' <title>The Shelter Helper</title>' +
+ ' <meta name="viewport" content="width=device-width, initial-scale=1.0"/>' +
+'</head>' +
+' <body style="font-family: Arial;color:#495864">' +
+' <table cellpadding="0" cellspacing="0" width="100%">' +
+'  <tr>' +
+'	<td align="center" bgcolor="#F5FAFA" style="padding: 40px 0 30px 0;">' +
+'	 <img src="https://theshelterhelper.com/images/logo.png"' +
+'		 alt="The Shelter Helper" width="300" style="display: block;" />' +
+'	</td>' +
+'</tr>' +
+'	<tr>' +
+'   <td style="padding: 60px;">' +
+'		%%MESSAGE_HERE%%' +
+'   </td>' +
+'   </tr>' +
+'   <tr>' +
+'   <td style="padding: 30px; color: #B7AFA3; background-color: #F5FAFA; font-size:10px; text-align: center">' +
+'		The Shelter Helper<br/>' +
+'		<a href="mailto:info@theshelterhelper.com" target="_blank">info@theshelterhelper.com</a> |' +
+'	   <a href="https://theshelterhelper.com/#/userSettingsPage">Manage your email preferences</a>' +
+'   </td>' +
+'  </tr>' +
+' </table>' +
+'</body> ' +
+'</html>';
+
 
 function GetFirebaseData(path, onSuccess) {
 	console.log('GetFirebaseData');
@@ -45,9 +77,10 @@ function SendMemberRequestPendingEmail(adminEmail) {
 	    subject: 'Member requests pending at The Shelter Helper', // Subject line
 	    text: 'A new member request is pending for your volunteer group at The Shelter Helper.  ' +
 			' Go to http://theshelterhelper.com/ and view the Members tab on your group to approve or deny.', // plaintext body
-	    html: 'A new member request is pending for your volunteer group at The Shelter Helper. ' +
-			' Go to <a href="http://theshelterhelper.com/">The Shelter Helper</a> and view the Members ' +
-			'tab on your group to approve or deny.' // html body
+	    html: htmlMessageCode.replace('%%MESSAGE_HERE%%', 'A new member request is pending for your ' +
+		' volunteer group at The Shelter Helper. Go to ' +
+		'<a href="http://theshelterhelper.com/">The Shelter Helper</a> and view the Members ' +
+		'tab on your group to approve or deny!') // html body
 	};
 
 	// send mail with defined transport object
@@ -69,9 +102,10 @@ function SendMemberRequestApprovedEmail(email, groupName) {
 	    subject: 'Your request to join a volunteer group was approved', // Subject line
 	    text: 'Your recent request to join a volunteer group at The Shelter Helper was approved. ' +
 			' Go to http://theshelterhelper.com/ to view your new group!  Congrats!', // plaintext body
-	    html: 'Your recent request to join the volunteer group ' + groupName + ' at The Shelter Helper was approved. ' +
-			' Visit <a href="http://theshelterhelper.com/">The Shelter Helper</a> to view your new group. ' +
-			'Congrats!' // html body
+	    html: htmlMessageCode.replace('%%MESSAGE_HERE%%',
+			'Your recent request to join the volunteer group ' + groupName + ' at The Shelter Helper was approved. ' +
+			' Visit <a href="http://theshelterhelper.com/">The Shelter Helper</a> to view your new group. <br/><br/>' +
+			'Congrats!') // html body
 	};
 
 	// send mail with defined transport object
