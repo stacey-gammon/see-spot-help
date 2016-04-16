910c537884dda8861164d6a86a7218236dca38fe
Stacey Gammon <stacey@staceygammon.com>
2016-04-16 19:52:44 -0400

    missing file

---
+++ SeeSpotHelp/SeeSpotHelp/App/stores/storestatehelper.ts
@@ -0,0 +1,34 @@
+
+import Permission from '../core/permission';
+import LoginStore from './loginstore';
+import PermissionsStore from './permissionsstore';
+
+class StoreStateHelper {
+	constructor() {}
+
+	public static GetPermission(state) {
+		var groupId = null;
+		if (state.groupId) {
+			groupId = state.groupId;
+		} else if (state.group) {
+			groupId = state.group.id;
+		}
+
+		return LoginStore.getUser() && groupId ?
+			PermissionsStore.getPermission(LoginStore.getUser().id, groupId) :
+			Permission.CreateNonMemberPermission();
+	}
+
+	public static AddChangeListeners(stores, reactClass) {
+		for (var i = 0; i < stores.length; i++) {
+			stores[i].addChangeListener(reactClass.onChange);
+		}
+	}
+	public static RemoveChangeListeners(stores, reactClass) {
+		for (var i = 0; i < stores.length; i++) {
+			stores[i].removeChangeListener(reactClass.onChange);
+		}
+	}
+}
+
+export default StoreStateHelper;
