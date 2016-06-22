ccc153c6adee309e25871167ef041781bcc4b8dd
Stacey Gammon <stacey@staceygammon.com>
2016-06-22 19:27:59 -0400

    break out firebase config int separate file

--- SeeSpotHelp/SeeSpotHelp/App/core/dataservices.ts
+++ SeeSpotHelp/SeeSpotHelp/App/core/dataservices.ts
@@ -2,25 +2,8 @@
 var Firebase = require('firebase');
 
 // Initialize Firebase
-var devConfig = {
-  apiKey: "AIzaSyCTMUD7ilylF5erNiGtsoSDp6kReVOBWXY",
-  authDomain: "shelter-helpers-dev.firebaseapp.com",
-  databaseURL: "https://shelter-helpers-dev.firebaseio.com",
-  storageBucket: "shelter-helpers-dev.appspot.com",
-};
-var releaseConfig = {
-  apiKey: "AIzaSyBZk2PjHJAM-HYyiUegWFhMnbbCfZAr_e4",
-  authDomain: "shelter-helpers-release.firebaseapp.com",
-  databaseURL: "https://shelter-helpers-release.firebaseio.com",
-  storageBucket: "shelter-helpers-release.appspot.com",
-};
-var liveConfig = {
-  apiKey: "AIzaSyAmUDL3vRmv7RsLJIe9oMKLqHhmvuOmBCc",
-  authDomain: "shining-torch-1432.firebaseapp.com",
-  databaseURL: "https://shining-torch-1432.firebaseio.com",
-  storageBucket: "shining-torch-1432.appspot.com",
-};
-Firebase.initializeApp(releaseConfig);
+import initFirebase from './firebaseconfig';
+initFirebase();
 
 export default class DataServices {
   private static database = Firebase.database();
